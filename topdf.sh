7438c073d7e7968d1a0d25ce9962ae2df539316e
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-08-02 16:27:43 -0500

    added new title also updated build script for Ubuntu 18.04

--- topdf.sh
+++ topdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#docbook=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns/fo/docbook.xsl 
+docbook=/usr/share/xml/docbook/stylesheet/docbook-xsl/fo/docbook.xsl 
 set -e
 cp src/fig/*png /tmp
 xmllint --xinclude src/primary-book/fulltext.xml -o /tmp/out.xml
-xsltproc /Applications/Oxygen\ XML\ Editor/frameworks/docbook/xsl/fo/docbook.xsl /tmp/out.xml > /tmp/out.fo
+xsltproc $docbook /tmp/out.xml > /tmp/out.fo
 fop -fo /tmp/out.fo book.pdf
